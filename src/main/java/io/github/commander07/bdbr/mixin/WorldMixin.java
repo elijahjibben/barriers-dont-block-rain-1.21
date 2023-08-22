d08ac27a4109f33f24a9bebfeb15fe7a9378e4d2
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-08-22 16:08:43 +0200

    fix: vanilla rain going through blocks

--- src/main/java/io/github/commander07/bdbr/mixin/WorldMixin.java
+++ src/main/java/io/github/commander07/bdbr/mixin/WorldMixin.java
@@ -1,29 +1,25 @@
 package io.github.commander07.bdbr.mixin;
 
-import net.minecraft.block.AirBlock;
 import net.minecraft.block.BarrierBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = World.class)
 public abstract class WorldMixin {
-    @Shadow public abstract BlockState getBlockState(BlockPos pos);
-
     @Inject(at = @At("HEAD"), method = "getTopY", cancellable = true)
     public void getTopPosition(Heightmap.Type heightmap, int x, int z, CallbackInfoReturnable<Integer> cir) {
         if (heightmap == Heightmap.Type.MOTION_BLOCKING) {
             int i = -1;
             for (int y = 0; y!=255; y++) {
-                Block block = this.getBlockState(new BlockPos(x, i, z)).getBlock();
-                if (!(block instanceof AirBlock) && !(block instanceof BarrierBlock)) {
+                BlockState state = MinecraftClient.getInstance().world.getBlockState(new BlockPos(x, y, z));
+                if ((state.blocksMovement() || !state.getFluidState().isEmpty()) && !(state.getBlock() instanceof BarrierBlock)) {
                     i = y;
                 }
             }
